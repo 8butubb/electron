14a3140758922840f097b5d73fef2cc56a737ccf
8butubb <66757505+8butubb@users.noreply.github.com>
2024-03-28 07:27:01 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,108 @@
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const mysql = require('mysql');
+const fs = require('fs');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+let mainWindow;
+let connection;
+
+function createConnection() {
+  connection = mysql.createConnection({
+    host: '192.168.1.65',
+    user: 'root',
+    password: 'kkxx2020',
+    database: 'jkwj'
+  });
+
+  connection.on('error', (err) => {
+    console.error('MySQL connection error:', err.message);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNREFUSED') {
+      console.log('Attempting to reconnect to MySQL...');
+      createConnection();
+    } else {
+      throw err;
+    }
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err.message);
+    } else {
+      console.log('Connected to MySQL database');
+    }
+  });
+}
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.webContents.openDevTools();
+
+  createConnection();
+
+  ipcMain.on('query-mysql', (event, { queries }) => {
+    const queryResults = [];
+
+    queries.forEach((query, index) => {
+      connection.query(query, (err, results) => {
+        if (err) {
+          event.sender.send('query-error', err.message);
+        } else {
+          queryResults.push({ index, results });
+          if (queryResults.length === queries.length) {
+            event.sender.send('query-results', queryResults);
+            saveDeviceDataToXmlFile(queryResults);
+          }
+        }
+      });
+    });
+  });
+
+  function saveDeviceDataToXmlFile(queryResults) {
+    let xmlContent = '<?xml version="1.0" encoding="UTF-8"?>\n';
+    xmlContent += '<DeviceManager version="2.0">\n';
+
+    queryResults.forEach(queryResult => {
+      queryResult.results.forEach(result => {
+        const deviceMatch = result.boby.match(/<Device.*?\/>/);
+        if (deviceMatch) {
+          xmlContent += `\t${deviceMatch[0]}\n`;
+        }
+      });
+    });
+
+    xmlContent += '</DeviceManager>';
+
+    // 显示保存对话框，获取用户选择的文件路径
+    dialog.showSaveDialog(mainWindow, {
+      defaultPath: '设备信息.xml', // 默认文件名
+      filters: [{ name: 'XML Files', extensions: ['xml'] }] // 文件类型过滤器
+    }).then((result) => {
+      if (!result.canceled) {
+        const filePath = result.filePath;
+        fs.writeFile(filePath, xmlContent, (err) => {
+          if (err) {
+            console.error('保存xml文件出错:', err.message);
+          } else {
+            console.log('设备信息已保存为xml文件:', filePath);
+          }
+        });
+      }
+    }).catch((err) => {
+      console.error('保存对话框出错:', err.message);
+    });
+  }
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+});
